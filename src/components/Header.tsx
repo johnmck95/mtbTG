79ce4fb7c4ab4c347705a3c23a528cf8887eb555
John <john.mic.mckinnon@gmail.com>
2021-11-30 22:00:37 -0800

    Overode basic create-app config, everything to be completed

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import react from "react"
+import {HStack, Heading} from "@chakra-ui/react"
+
+export default function Header() {
+    return(
+        <HStack>
+            <Heading>Mountain Bike Tuning Guide</Heading>
+        </HStack>
+
+    )
+}
