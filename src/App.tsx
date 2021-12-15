6124b15d5803b19e2395c9668bf52606707ceac5
John <john.mic.mckinnon@gmail.com>
2021-12-14 17:52:11 -0800

    Got the background image loaded, created an images directory in src

--- src/App.tsx
+++ src/App.tsx
@@ -9,32 +9,14 @@ import {
   Grid,
   theme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
 import Header from "./components/Header"
+import "./app_style.css"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Header />
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <body className="content">
+      
+    </body>
   </ChakraProvider>
 )
