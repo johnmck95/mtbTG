63eb941206380d015f7676a14b7b52acbd7dfb54
John <john.mic.mckinnon@gmail.com>
2021-12-17 21:34:54 -0800

    Minor formatting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,20 +9,19 @@ export default function Header({pageNumber}: HeaderProps) {
         <Box as="header" bg="black" color="white">
             <Container bg="black" maxW={['56.25rem']} p="0px">
                 <Flex justify="space-between" alignItems="center" px="10px" h='50px'>
-                        <Link href="/">
-                            <Heading as='h1' fontSize={[null, 'md', 'lg', 'xl']} textDecoration="underline" m="5px">MTB TUNING GUIDE</Heading>
-                        </Link>
-                        <Stack direction={['column', 'row']} fontSize='sm'  spacing={['0em','3rem', '4rem', '5rem']} alignItems="center">
-                            <ButtonGroup spacing="1rem">
-                                <Link href="/about">ABOUT</Link>
-                                <Text> | </Text>
-                                <Link href="/help">HELP</Link>              
-                            </ButtonGroup>
-                            {pageNumber > 0 &&
-                                <Text fontSize="xs">Page {pageNumber} of 3</Text>
-                            }
-                        </Stack>
-
+                    <Link href="/">
+                        <Heading as='h1' fontSize={[null, 'md', 'lg', 'xl']} textDecoration="underline" m="5px">MTB TUNING GUIDE</Heading>
+                    </Link>
+                    <Stack direction={['column', 'row']} fontSize='sm'  spacing={['0em','3rem', '4rem', '5rem']} alignItems="center">
+                        <ButtonGroup spacing="1rem">
+                            <Link href="/about">ABOUT</Link>
+                            <Text> | </Text>
+                            <Link href="/help">HELP</Link>              
+                        </ButtonGroup>
+                        {pageNumber > 0 &&
+                            <Text fontSize="xs">Page {pageNumber} of 3</Text>
+                        }
+                    </Stack>
                 </Flex>
             </Container>
         </Box>
