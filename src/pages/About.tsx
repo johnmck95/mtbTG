1eb4d8235951624d99756a8dd258cb5ea75257f0
John <john.mic.mckinnon@gmail.com>
2021-12-18 21:24:16 -0800

    code cleaning

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,23 +1,23 @@
 import Header from "../components/Header"
 import {Divider, Image, Text, Stack, VStack, Heading, AspectRatio, Container, List, ListItem, ListIcon} from "@chakra-ui/react"
 import headshot from "../images/headshot.jpg"
-import { MdOutlineDirectionsBike } from "react-icons/md";
+import {MdOutlineDirectionsBike} from "react-icons/md";
 import {FaLaptopCode} from "react-icons/fa"
 
 export default function About() {
     return(
         <>
             <Header pageNumber={0}/>
-            <Container maxW={"50rem"} >
+            <Container maxW="50rem" >
                 <Stack direction={['column', 'row']} spacing="2rem" alignItems="flex-start" my="3rem">
-                    <Image src={headshot} w="14rem" maxW="50%" borderRadius="6px" m="10px" alignSelf={"center"}/>
+                    <Image src={headshot} w="14rem" maxW="50%" borderRadius="6px" m="10px" alignSelf="center"/>
                     <VStack alignItems="flex-start"  m="10px">
-                        <Heading as="h2" size="2xl" alignSelf={"center"} textAlign={"center"}> John McKinnon</Heading>
+                        <Heading as="h2" size="2xl" alignSelf="center" textAlign="center"> John McKinnon</Heading>
                         <Divider borderColor="brand.white"/>
-                        <Heading as="h4" size="l" textAlign={"center"} >
+                        <Heading as="h4" size="l" textAlign="center" >
                             3<sup>rd</sup> year Software Engineering Student at the University of Victoria
                         </Heading>
-                        <Text paddingTop="1.5rem" alignSelf={"center"} textDecoration={"underline"}>
+                        <Text paddingTop="1.5rem" alignSelf="center" textDecoration="underline">
                             The Tuning Guide was built for two reasons.
                         </Text>
                         <List>
@@ -33,7 +33,7 @@ export default function About() {
                     </VStack>
                 </Stack>
                 <Divider borderColor="brand.white"/>
-                    <Text  textAlign="justify" textJustify="inter-word" paddingTop={"2rem"}>
+                    <Text textAlign="justify" textJustify="inter-word" paddingTop="2rem">
                         Growing up on Vancouver Island, I have been a mountain biker for as long as I can remember.
                         I raced downhill while attending high school, then moved to Whister the day after graduation.
                         Since then, I have lived in Squamish, Vancouver, Victoria and New Zealand working in the bike insutry 
@@ -44,7 +44,7 @@ export default function About() {
                     </Text>
                     <AspectRatio my="3rem" maxW="768px">
                         <iframe
-                            title='John McKinnon || Self-Shot'
+                            title="John McKinnon || Self-Shot"
                             src="https://www.youtube.com/embed/iF4yQ3PL_tY"
                             allowFullScreen
                         />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return(
         <main className="home--background">
             <Header pageNumber={1}/>
-            <VStack alignSelf="center" justify={"center"} h="calc(100% - 50px)">
+            <VStack alignSelf="center" justify="center" h="calc(100% - 50px)">
                 <ChooseSetupGuide />
             </VStack>
         </main>
