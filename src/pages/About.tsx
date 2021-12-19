ffe7dd67bfdc36b09b2edeee5f197861a315ccfd
John <john.mic.mckinnon@gmail.com>
2021-12-18 21:09:12 -0800

    minor

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,15 +1,16 @@
 import Header from "../components/Header"
-import {Divider, Image, Text, HStack, VStack, Heading, AspectRatio, Container, List, ListItem, ListIcon} from "@chakra-ui/react"
+import {Divider, Image, Text, Stack, VStack, Heading, AspectRatio, Container, List, ListItem, ListIcon} from "@chakra-ui/react"
 import headshot from "../images/headshot.jpg"
 import { MdOutlineDirectionsBike } from "react-icons/md";
+import {FaLaptopCode} from "react-icons/fa"
 
 export default function About() {
     return(
         <>
             <Header pageNumber={0}/>
             <Container maxW={"50rem"} >
-                <HStack spacing="2rem" alignItems="flex-start" marginTop="50px">
-                    <Image src={headshot} w="14rem" maxW="50%" borderRadius="6px" m="10px"/>
+                <Stack direction={['column', 'row']} spacing="2rem" alignItems="flex-start" my="3rem">
+                    <Image src={headshot} w="14rem" maxW="50%" borderRadius="6px" m="10px" alignSelf={"center"}/>
                     <VStack alignItems="flex-start"  m="10px">
                         <Heading as="h2" size="2xl" alignSelf={"center"} textAlign={"center"}> John McKinnon</Heading>
                         <Divider borderColor="brand.white"/>
@@ -25,12 +26,13 @@ export default function About() {
                                 To help mountain bikers get the most out of their products
                             </ListItem>
                             <ListItem>
-                                <ListIcon as={MdOutlineDirectionsBike} color="brand.blue"/>
-                                To build a portoflio to show future employers.
+                                <ListIcon as={FaLaptopCode} color="brand.blue"/>
+                                To build a portoflio to show future employers
                             </ListItem>
                         </List>
                     </VStack>
-                </HStack>
+                </Stack>
+                <Divider borderColor="brand.white"/>
                     <Text  textAlign="justify" textJustify="inter-word" paddingTop={"2rem"}>
                         Growing up on Vancouver Island, I have been a mountain biker for as long as I can remember.
                         I raced downhill while attending high school, then moved to Whister the day after graduation.
@@ -40,7 +42,7 @@ export default function About() {
                         Bikes Racing Team, and living full time in Victoria completing my Software Engineering degree and riding one 
                         of my many bikes.
                     </Text>
-                    <AspectRatio marginTop="3rem" maxW="768px">
+                    <AspectRatio my="3rem" maxW="768px">
                         <iframe
                             title='John McKinnon || Self-Shot'
                             src="https://www.youtube.com/embed/iF4yQ3PL_tY"
