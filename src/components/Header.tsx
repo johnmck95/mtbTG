038b67afa95024aeab26a8de93fcf948091fa942
John <john.mic.mckinnon@gmail.com>
2021-12-20 13:24:10 -0800

    small code cleanup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,13 +8,11 @@ export default function Header() {
     // useViewport from: https://blog.logrocket.com/developing-responsive-layouts-with-react-hooks/
     const useViewport = () => {
         const [width, setWidth] = React.useState(window.innerWidth);
-
         React.useEffect(() => {
           const handleWindowResize = () => setWidth(window.innerWidth);
           window.addEventListener("resize", handleWindowResize);
           return () => window.removeEventListener("resize", handleWindowResize);
         }, []);
-        // Return the width so we can use it in our components
         return { width };
       }
 
