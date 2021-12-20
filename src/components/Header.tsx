f008a40edce7e08e47912b76dccebf1b85acb4bc
John <john.mic.mckinnon@gmail.com>
2021-12-20 13:23:39 -0800

    Changed the header to use a canva image as the title, implemented a cusotm hook to change the header from showing About|Help to the hamburger icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,49 @@
-import {Flex, Box, Heading, ButtonGroup, Link, Text, HStack, Container} from "@chakra-ui/react"
-
+import {Flex, Box, ButtonGroup, Link, Text, HStack, Container, Image, Icon} from "@chakra-ui/react"
+import {FaBars} from "react-icons/fa"
+import MtbTG from "../images/mtbTG-logo.png"
+import React from "react"
 
 export default function Header() {
+
+    // useViewport from: https://blog.logrocket.com/developing-responsive-layouts-with-react-hooks/
+    const useViewport = () => {
+        const [width, setWidth] = React.useState(window.innerWidth);
+
+        React.useEffect(() => {
+          const handleWindowResize = () => setWidth(window.innerWidth);
+          window.addEventListener("resize", handleWindowResize);
+          return () => window.removeEventListener("resize", handleWindowResize);
+        }, []);
+        // Return the width so we can use it in our components
+        return { width };
+      }
+
+      const { width } = useViewport()
+      const breakpoint = 520;
+
     return(
         <Box as="header" w="100%"  position="fixed" top="0" bg="brand.black" color="brand.white" zIndex="200">
             <Container bg="black" maxW="50rem" p="0px">
-                <Flex justify="space-between" alignItems="center" px="10px" h='50px'>
+                <Flex justify="space-between" alignItems="center" px="8px" h='50px'>
                     <Link href="/">
-                        <Heading as='h1' fontSize={[null, 'md', 'lg', 'xl']} textDecoration="underline" m="5px">MTB TUNING GUIDE</Heading>
+                        <Image src={MtbTG} h={["30px", "40px"]} />
                     </Link>
-                    <HStack fontSize='sm'  spacing={['0em','3rem', '4rem', '5rem']} alignItems="center">
-                        <ButtonGroup spacing="1rem">
-                            <Link href="/about">ABOUT</Link>
-                            <Text> | </Text>
-                            <Link href="/help">HELP</Link>              
-                        </ButtonGroup>
-                    </HStack>
+                    { width >= breakpoint &&
+                        <HStack fontSize='sm'  spacing={['0em','3rem', '4rem', '5rem']} alignItems="center" mr="10px">
+                            <ButtonGroup spacing="1rem">
+                                <Link href="/about">ABOUT</Link>
+                                <Text> | </Text>
+                                <Link href="/help">HELP</Link>              
+                            </ButtonGroup>
+                        </HStack>
+                    }
+                    { width < breakpoint &&
+                        <Icon as={FaBars} w={6} h={6} mr="10px"/>
+                    }
                 </Flex>
             </Container>
         </Box>
     )
 }
+
+{/* <Icon as={FaBars} /> */}
