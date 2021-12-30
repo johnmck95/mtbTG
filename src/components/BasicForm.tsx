adbf74f38c92ccf812bd0755e071c6e1a9cb86be
John <john.mic.mckinnon@gmail.com>
2021-12-29 19:02:32 -0800

    Hooked up the metric/imperial button on Bike Metrics

--- src/components/BasicForm.tsx
+++ src/components/BasicForm.tsx
@@ -2,6 +2,9 @@ import {useState} from "react"
 import {Button, Flex, VStack, Input, Container, SimpleGrid, GridItem, FormControl, FormLabel, Select, Heading, Divider, Box, HStack } from "@chakra-ui/react"
 
 export default function BasicForm() {
+    const [imperialRider, setImperialRider] = useState(true)
+    const [imperialBike, setImperialBike] = useState(false)
+
     const [inputs, setInputs] = useState({
         height: "",
         bikeType: "",
@@ -10,12 +13,9 @@ export default function BasicForm() {
         stack: ""
     })
 
-    const [imperialRider, setImperialRider] = useState(true)
-
     return(
         <Container  maxW="37.5rem">
             <VStack bg="brand.darkGrey" borderRadius="16px" pb="20px">
-
                 <Flex position="relative" justifyContent={["space-around", "center"]} w="100%">
                     <Heading as='h2' textAlign="center" fontSize={["xl", "2xl"]} color="brand.white" maxW="80%" ml={[-4, 0]} marginTop={["1rem", "1.5rem","2rem"]} > RIDER METRICS </Heading>
                     <Box >
@@ -42,7 +42,6 @@ export default function BasicForm() {
                         </Button>
                     </Box>
                 </Flex>
-
                 <Divider orientation='horizontal' borderColor="brand.white" size="xl" maxW="95%" marginBottom="8rem"/>
                 <Container maxW={["85%", "75%"]}>
                     {imperialRider &&
@@ -94,25 +93,50 @@ export default function BasicForm() {
                     </SimpleGrid>
                     </Container>
 
-                    <Heading as='h2' textAlign="center" fontSize="2xl" color="brand.white" maxW="80%" marginTop={["1rem", "1.5rem","2rem"]} > BIKE METRICS </Heading>
+                     <Flex position="relative" justifyContent={["space-around", "center"]} w="100%">
+                        <Heading as='h2' textAlign="center" fontSize={["xl", "2xl"]} color="brand.white" maxW="80%" ml={[-4, 0]} marginTop={["1rem", "1.5rem","2rem"]} > BIKE METRICS </Heading>
+                        <Box >
+                            <Button 
+                                position="absolute" 
+                                right="65px" 
+                                size="xs" 
+                                marginTop={["1rem", "1.5rem","2rem"]}
+                                zIndex={imperialBike? 0 : 1}
+                                bg={imperialBike? "brand.lightGrey" : "brand.blue"} 
+                                onClick={() => setImperialBike(prevImperialBike => !prevImperialBike)}>
+                                    Metric
+                            </Button>
+                            <Button 
+                                position="absolute" 
+                                right="10px" 
+                                size="xs" 
+                                variant="ghost" 
+                                marginTop={["1rem", "1.5rem","2rem"]}
+                                zIndex={imperialRider? 1 : 0}
+                                bg={imperialBike? "brand.blue": "brand.lightGrey"} 
+                                onClick={() => setImperialBike(prevImperialBike => !prevImperialBike)}>
+                                    Imperial
+                            </Button>
+                        </Box>
+                    </Flex>
+
                     <Divider orientation='horizontal' borderColor="brand.white" size="xl" maxW="95%" marginBottom="8rem"/>
                     <Container maxW={["85%", "75%"]}>
                         <SimpleGrid columns={2} columnGap={4} pb={3}>
                             <GridItem colSpan={1}>
                                 <FormControl>
-                                    <FormLabel fontSize={["xs", "sm", "md"]}>Reach (mm)</FormLabel>
-                                    <Input placeholder="510" maxWidth={24}/>
+                                    <FormLabel fontSize={["xs", "sm", "md"]}>Reach {imperialBike? "(inches)" : "(mm)"}</FormLabel>
+                                    <Input placeholder={imperialBike? "20.1" : "510"} maxWidth={24}/>
                                 </FormControl>
                             </GridItem>
                             <GridItem colSpan={1}>
                                 <FormControl>
-                                    <FormLabel fontSize={["xs", "sm", "md"]}>Stack (mm)</FormLabel>
-                                    <Input placeholder="640" maxWidth={24}/>
+                                    <FormLabel fontSize={["xs", "sm", "md"]}>Stack {imperialBike? "(inches)" : "(mm)"}</FormLabel>
+                                    <Input placeholder={imperialBike? "25.2" : "640"} maxWidth={24}/>
                                 </FormControl>
                             </GridItem>
                         </SimpleGrid>
                     </Container>
-
                     <Button w={36} bg="brand.blue" borderRadius='50px'> Next </Button>
             </VStack>
         </Container> 
