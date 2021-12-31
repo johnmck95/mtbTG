3470874929c90612d5ef13803acb161fbd58add6
John <john.mic.mckinnon@gmail.com>
2021-12-31 14:04:05 -0800

    State appears to be properly hooked up. Toggling between units will auto update the other fields for both rider and bike metrics

--- src/components/BasicForm.tsx
+++ src/components/BasicForm.tsx
@@ -17,28 +17,69 @@ export default function BasicForm() {
         bikeType: ""
     })
 
+    function toggleRiderUnit() {
+        setImperialRider(prevImperialRider => !prevImperialRider)
+        stateRiderConversion()
+    }
 
-    // const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    //     console.log(event)
-    //     const {name, value} = event.target
-    //     setInputs(prevInputs => ({
-    //         ...prevInputs,
-    //         [name] : value,
-    //     }))
-    //   };
+    function toggleBikeUnit(){
+        setImperialBike(prevImperialBike => !prevImperialBike)
+        stateBikeConversion()
+    }
 
-    function handleChange(event: ChangeEvent<HTMLInputElement>) {
-        // event.preventDefault();
-        // console.log(typeof event)
-
-        
+    function handleChange(event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) {
         const {name, value} = event.target
-
         setInputs(prevInputs => ({
             ...prevInputs,
             [name] : value,
+        }))        
+    }
+
+     // NOTE: watch out for 'e' in the input
+    function stateRiderConversion() {
+        let heightCMCalc = 0
+        let heightFootCalc = 0
+        let heightInchesCalc = 0   
+
+        if (inputs.heightFeet !== "" && inputs.heightInches !== "")
+             heightCMCalc = parseFloat(inputs.heightFeet) * 30.48 + parseFloat(inputs.heightInches) * 2.54
+        if (inputs.heightCM !== ""){
+            const totalInches = parseFloat(inputs.heightCM) / 2.54
+            heightFootCalc = Math.floor(totalInches / 12)
+            heightInchesCalc = (totalInches % 12)          
+        }
+
+        setInputs(prevInputs => ({
+            ...prevInputs,
+            heightCM: heightCMCalc !== 0? heightCMCalc.toFixed(1) : inputs.heightCM,
+            heightFeet: heightFootCalc !== 0? heightFootCalc.toFixed(0) : inputs.heightFeet,
+            heightInches: heightInchesCalc !== 0? heightInchesCalc.toFixed(0) : inputs.heightInches
         }))
-        console.log(inputs)
+    }
+
+    // NOTE: watch out for 'e' in the input
+    function stateBikeConversion() {
+        let reachMMCalc = 0
+        let reachInchCalc = 0
+        let stackMMCalc = 0
+        let stackInchCalc = 0
+
+        if (inputs.reachMM !== "")
+            reachInchCalc = parseFloat(inputs.reachMM)/25.4
+        if (inputs.stackMM !== "")
+            stackInchCalc = parseFloat(inputs.stackMM)/25.4
+        if (inputs.reachInches !== "")
+            reachMMCalc = parseFloat(inputs.reachInches)*25.4
+        if(inputs.stackInches !== "")
+            stackMMCalc = parseFloat(inputs.stackInches)*25.4
+
+        setInputs( prevInputs => ({
+            ...prevInputs,
+            reachMM: reachMMCalc !== 0? reachMMCalc.toFixed(0) : inputs.reachMM,
+            reachInches: reachInchCalc !== 0? reachInchCalc.toFixed(1) : inputs.reachInches,
+            stackMM: stackMMCalc !== 0? stackMMCalc.toFixed(0) : inputs.stackMM,
+            stackInches: stackInchCalc !== 0? stackInchCalc.toFixed(1) : inputs.stackInches
+        }))
     }
 
     return(
@@ -55,7 +96,7 @@ export default function BasicForm() {
                             marginTop={["1rem", "1.5rem","2rem"]}
                             zIndex={imperialRider? 0 : 1}
                             bg={imperialRider? "brand.lightGrey" : "brand.blue"} 
-                            onClick={() => setImperialRider(prevImperialRider => !prevImperialRider)}>
+                            onClick={toggleRiderUnit}>
                                 Metric
                         </Button>
                         <Button 
@@ -67,7 +108,7 @@ export default function BasicForm() {
                             marginTop={["1rem", "1.5rem","2rem"]}
                             zIndex={imperialRider? 1 : 0}
                             bg={imperialRider? "brand.blue": "brand.lightGrey"} 
-                            onClick={() => setImperialRider(prevImperialRider => !prevImperialRider)}>
+                            onClick={toggleRiderUnit}>
                                 Imperial
                         </Button>
                     </Box>
@@ -83,9 +124,10 @@ export default function BasicForm() {
                                         placeholder="6" 
                                         maxWidth={24} 
                                         focusBorderColor='brand.blue'
+                                        type="number"
+                                        onChange={handleChange}
                                         value={inputs.heightFeet}
                                         name={"heightFeet"}
-                                        onChange={handleChange}
                                         />
                                 </FormControl>
                             </GridItem>
@@ -96,6 +138,7 @@ export default function BasicForm() {
                                         placeholder="2" 
                                         maxWidth={24} 
                                         focusBorderColor='brand.blue'
+                                        type="number"
                                         value={inputs.heightInches}
                                         name={"heightInches"}
                                         onChange={handleChange}
@@ -109,12 +152,13 @@ export default function BasicForm() {
                                 <FormControl>
                                     <FormLabel fontSize={["xs", "sm", "md"]} mx={0} mb="2px">Height (cm)</FormLabel>
                                     <Input 
-                                    placeholder="187" 
-                                    maxWidth={24} 
-                                    focusBorderColor='brand.blue'
-                                    value={inputs.heightCM}
-                                    name={"heightCM"}
-                                    onChange={handleChange}
+                                        placeholder="187" 
+                                        maxWidth={24} 
+                                        focusBorderColor='brand.blue'
+                                        type="number"
+                                        value={inputs.heightCM}
+                                        name={"heightCM"}
+                                        onChange={handleChange}
                                     />
                                 </FormControl>
                             </GridItem>
@@ -124,20 +168,21 @@ export default function BasicForm() {
                         <GridItem colSpan={1}>
                             <FormControl>
                                 <FormLabel fontSize={["xs", "sm", "md"]} mx={0} mb="2px">Weight Bias</FormLabel>
-                                <Select 
+                                <Select
+                                    id="weightBias"
                                     focusBorderColor='brand.blue' 
                                     name="weightBias"  
-                                    // onChange={handleChange}
-                                    > 
-                                    <option value={inputs.weightBias}> Neutral </option>
-                                    <option value={inputs.weightBias}> Rearward </option>
-                                    <option value={inputs.weightBias}> Forward </option>
+                                    onChange={e => handleChange(e)}
+                                    value={inputs.weightBias}> 
+                                        <option value="">-- Choose --</option>
+                                        <option value="neutral"> Neutral </option>
+                                        <option value="rearward"> Rearward </option>
+                                        <option value="forward"> Forward </option>
                                 </Select>
                             </FormControl>
                         </GridItem>
                     </SimpleGrid>
                     </Container>
-
                      <Flex position="relative" justifyContent={["space-around", "center"]} w="100%">
                         <Heading as='h2' textAlign="center" fontSize={["xl", "2xl"]} color="brand.white" maxW="80%" ml={[-4, 0]} marginTop={["1rem", "1.25rem"]} > BIKE METRICS </Heading>
                         <Box >
@@ -149,7 +194,7 @@ export default function BasicForm() {
                                 marginTop={["1rem", "1.5rem","2rem"]}
                                 zIndex={imperialBike? 0 : 1}
                                 bg={imperialBike? "brand.lightGrey" : "brand.blue"} 
-                                onClick={() => setImperialBike(prevImperialBike => !prevImperialBike)}>
+                                onClick={toggleBikeUnit}>
                                     Metric
                             </Button>
                             <Button 
@@ -161,12 +206,11 @@ export default function BasicForm() {
                                 marginTop={["1rem", "1.5rem","2rem"]}
                                 zIndex={imperialBike? 1 : 0}
                                 bg={imperialBike? "brand.blue": "brand.lightGrey"} 
-                                onClick={() => setImperialBike(prevImperialBike => !prevImperialBike)}>
+                                onClick={toggleBikeUnit}>
                                     Imperial
                             </Button>
                         </Box>
                     </Flex>
-
                     <Divider orientation='horizontal' borderColor="brand.white" size="xl" maxW="95%" marginBottom="8rem"/>
                     <Container maxW={["85%", "75%"]}>
                         <SimpleGrid columns={2} columnGap={4} pb={3}>
@@ -177,6 +221,7 @@ export default function BasicForm() {
                                         placeholder={imperialBike? "20.1" : "510"} 
                                         maxWidth={24} 
                                         focusBorderColor='brand.blue'
+                                        type="number"
                                         value={imperialBike? inputs.reachInches : inputs.reachMM}
                                         name={imperialBike? "reachInches" : "reachMM"}
                                         onChange={handleChange}
@@ -190,24 +235,26 @@ export default function BasicForm() {
                                         placeholder={imperialBike? "25.2" : "640"} 
                                         maxWidth={24} 
                                         focusBorderColor='brand.blue'
+                                        type="number"
                                         value={imperialBike? inputs.stackInches : inputs.stackMM}
                                         name={imperialBike? "stackInches" : "stackMM"}
                                         onChange={handleChange}
                                         />
                                 </FormControl>
                             </GridItem>
-
                             <GridItem colSpan={2} pb={1}>
                             <FormControl>
                                 <FormLabel fontSize={["xs", "sm", "md"]} mx={0} mb="2px">Bike Type</FormLabel>
                                 <Select 
                                     focusBorderColor='brand.blue' 
-                                    name="inputs.bikeType"
-                                    // onChange={handleChange}
+                                    name="bikeType"
+                                    value={inputs.bikeType}
+                                    onChange={e => handleChange(e)}
                                     > 
-                                    <option value={inputs.bikeType}> Enduro </option>
-                                    <option value={inputs.bikeType}> Trail </option>
-                                    <option value={inputs.bikeType}> Cross Country</option>
+                                    <option value="">-- Choose --</option>
+                                    <option value="enduro"> Enduro </option>
+                                    <option value="trail"> Trail </option>
+                                    <option value="xc"> Cross Country</option>
                                 </Select>
                             </FormControl>
                         </GridItem>
