7bd000d91904b633c29c1102d29d0387fb45cb3a
John <john.mic.mckinnon@gmail.com>
2022-01-20 22:18:15 -0800

    Centered modal

--- src/components/LearnMoreModal.tsx
+++ src/components/LearnMoreModal.tsx
@@ -24,7 +24,7 @@ export default function LearnMoreModal({id}: LeanMoreModalProps) {
                 > learn more 
             </Button>
         
-            <Modal isOpen={isOpen} onClose={onClose}>
+            <Modal isOpen={isOpen} onClose={onClose} isCentered>
                 <ModalOverlay />
                 <ModalContent bg='brand.darkGrey'>
                     <ModalHeader textDecoration="underline" color="brand.white">{data.title}</ModalHeader>
