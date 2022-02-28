866d503455c4f085977c43236e735709f589a4a6
John <john.mic.mckinnon@gmail.com>
2022-02-28 09:28:58 -0800

    Made a simple linear function in excel based on Evan and my handlebar preferences and applied it to the tuning guide. Still need more data points to increase function accuracy

--- src/algorithms/Algorithm.ts
+++ src/algorithms/Algorithm.ts
@@ -12,19 +12,26 @@ interface AlgorithmProps{
 
 function Algorithm({heightFeet, heightInches, heightCM, handling, reachInches, reachMM, stackInches, stackMM, bikeType}: AlgorithmProps) {
     const outputs = {
-        barWidthMM: "780",
-        barWidthInch: "30.7",
-        barRiseMM: "25",
-        barRiseInch: "1.0",
-        stemLengthMM: "50",
-        stemLengthInch: "2.0",
-        spacersMM: "10",
-        spacersInch: "0.4",
-        frontTirePSI: "22",
-        rearTirePSI: "25",
-        inserts: "yes"
+        barWidthMM: "",
+        barWidthInch: "",
+        barRiseMM: "",
+        barRiseInch: "",
+        stemLengthMM: "",
+        stemLengthInch: "",
+        spacersMM: "",
+        spacersInch: "",
+        frontTirePSI: "",
+        rearTirePSI: "",
+        inserts: "" // yes/no
 
     }
+    function barWidthCalc(){
+        const riderHeight = parseInt(heightCM)
+        outputs.barWidthMM = (riderHeight * 0.7692 + 625.38).toFixed(0)
+        outputs.barWidthInch = ((riderHeight * 0.7692 + 625.38) / 25.4).toFixed(1)
+    }
+    
+    barWidthCalc()
     return outputs
 }
 
