e9e61d22e1233e777bd1da8bc4fb1f5ffad1950b
John <john.mic.mckinnon@gmail.com>
2022-02-27 21:48:34 -0800

    Updated test messages

--- src/components/ErrorAlert/__test__/ErrorAlert.test.tsx
+++ src/components/ErrorAlert/__test__/ErrorAlert.test.tsx
@@ -2,12 +2,12 @@ import { render } from "@testing-library/react";
 import ErrorAlert from './../ErrorAlert'
 import {errorCodes} from "./../../../data/ErrorCodes"
 
-test("error alert renders without error", () => {
+test("ErrorAlert renders without error", () => {
     const { getByText } = render(<ErrorAlert errorMessage="test message" />);
     getByText("test message");
   });
 
-test("correctly reads from ErrorCodes.ts", () =>{
+test("ErrorAlert correctly reads from ErrorCodes.ts", () =>{
     const { getByText } = render(<ErrorAlert errorMessage={errorCodes[1].errorMessage} />);
     getByText("Please enter a postive value less than 12 for Height (inches)")
 })
