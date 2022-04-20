3ae80b26dfc51198b27a17ff3186d6f1b0d284be
John <john.mic.mckinnon@gmail.com>
2022-04-20 09:54:59 -0700

    Cleaned up algorithm.test.tsx for beginning of testing

--- src/algorithm/Algorithm/__test__/Algorithm.test.tsx
+++ src/algorithm/Algorithm/__test__/Algorithm.test.tsx
@@ -1,11 +1,6 @@
 import { render } from "@testing-library/react";
 
-// test("tireInsertCalc() returns 0 for beginners", () => {
+test("tireInsertCalc() returns 0 for beginners", () => {
    
-//   });
+  });
 
-it ('tester test', () => {
-    const a: string = 10;
-    expect(a).toBe(10)
-    // expect(5+5).toBe(10)
-})
