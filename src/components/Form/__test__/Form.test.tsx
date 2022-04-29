e054a2a84fe0685cbd11221c563ead1772cfdc53
John <john.mic.mckinnon@gmail.com>
2022-04-29 08:39:53 -0700

    Completed the metric/imperial tests for both RiderMetrics and BikeMetrics

--- src/components/Form/__test__/Form.test.tsx
+++ src/components/Form/__test__/Form.test.tsx
@@ -154,7 +154,7 @@ describe("Converting Rider Metrics from Imperial to Metic ", () => {
         ['5', '11', '180.3400'],
         ['5', '11.98', '182.8292'],
         ['6', '6', '198.1200'],
-    ])('should yield %i\'%f = %f', (feetIn, inchesIn, cmOut) => {
+    ])('should yield %i\'%f" = %fcm', (feetIn, inchesIn, cmOut) => {
         user.type(screen.getByLabelText("Height (feet)"), feetIn)
         user.type(screen.getByLabelText("Height (inches)"), inchesIn)
         user.click(screen.getByTestId("imperialRiderButton"))
@@ -179,7 +179,7 @@ describe("Converting Rider Metrics from Imperial to Metic ", () => {
             ['5', '11', '5', '11.0000'],
             ['5', '11.98', '5', '11.9800'],
             ['6', '6', '6', '6.0000']
-        ])('the original result of %f\'%f', (feetIn, inchesIn, feetOut, inchesOut) => {
+        ])('the original result of %f\'%f"', (feetIn, inchesIn, feetOut, inchesOut) => {
             user.type(screen.getByLabelText("Height (feet)"), feetIn)
             user.type(screen.getByLabelText("Height (inches)"), inchesIn)
             user.click(screen.getByTestId("imperialRiderButton"))
@@ -193,7 +193,7 @@ describe("Converting Rider Metrics from Imperial to Metic ", () => {
             ['100', '99.9999'],
             ['182.1', '182.1001'],
             ['240', '240.0001']
-        ])('the newly calculated (and very close) value of %f', (weightLBIn, weightLBOut) => {
+        ])('the close to original value of %flb', (weightLBIn, weightLBOut) => {
             user.type(screen.getByLabelText("Weight (lb)"), weightLBIn)
             user.click(screen.getByTestId("imperialRiderButton"))
             user.click(screen.getByTestId("imperialRiderButton"))
@@ -209,8 +209,57 @@ describe("Converting Bike Metrics from Metic to Imperial",  () => {
         render(<Home/>)
     })
     test.each([
-        ['475', '18.7008']
-    ])('should yield %f = %f', (mmIn, inchesOut) => {
+        ['400','15.7480'],
+        ['400.10', '15.7520'],
+        ['475', '18.7008'],
+        ['499.9999', '19.6850'],
+        ['549.67', '21.6406'],
+        ['550', '21.6535']
+    ])('should yield Reach values %fmm = %f"', (reachMMIn, reachInchesOut) => {
+        user.type(screen.getByLabelText("Reach (mm)"), reachMMIn)
+        user.click(screen.getByTestId("imperialBikeButton"))
+        expect(screen.getByDisplayValue(reachInchesOut)).toBeInTheDocument()
+    })
+    test.each([
+        ['550','21.6535'],
+        ['550.123', '21.6584'],
+        ['600.001', '23.6221'],
+        ['640', '25.1969'],
+        ['679.99', '26.7713'],
+        ['680', '26.7717']
+    ])('should yield Stack values %fmm = %f"', (stackMMIn, stackInchesOut) => {
+        user.type(screen.getByLabelText("Stack (mm)"), stackMMIn)
+        user.click(screen.getByTestId("imperialBikeButton"))
+        expect(screen.getByDisplayValue(stackInchesOut)).toBeInTheDocument()
+    })
 
+    describe("Then converting back to Metric from Imperial should have ", () => {
+        test.each([
+            ['400','399.9992'],
+            ['400.10', '400.1008'],
+            ['475', '475.0003'],
+            ['499.9999', '499.9990'],
+            ['549.67', '549.6712'],
+            ['550', '549.9989']
+        ])('the close to original Reach values %fmm = %fmm', (reachMMIn, reachMMOut) => {
+            user.type(screen.getByLabelText("Reach (mm)"), reachMMIn)
+            user.click(screen.getByTestId("imperialBikeButton"))
+            user.click(screen.getByTestId("imperialBikeButton"))
+            expect(screen.getByDisplayValue(reachMMOut)).toBeInTheDocument()
+        })
+
+        test.each([
+            ['500','499.9990'],
+            ['500.123', '500.1235'],
+            ['600.001', '600.0013'],
+            ['640', '640.0013'],
+            ['679.99', '679.9910'],
+            ['680', '680.0012']
+        ])('the close to original Stack values %fmm = %fmm', (stackMMIn, stackMMOut) => {
+            user.type(screen.getByLabelText("Stack (mm)"), stackMMIn)
+            user.click(screen.getByTestId("imperialBikeButton"))
+            user.click(screen.getByTestId("imperialBikeButton"))
+            expect(screen.getByDisplayValue(stackMMOut)).toBeInTheDocument()
+        })
     })
 })
