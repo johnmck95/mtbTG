bcf5f215d100eb599478f1f75c951eb5759cd70b
John <john.mic.mckinnon@gmail.com>
2022-05-07 18:49:23 -0700

    Minor code cleanup since last commit

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ export const App = () => (
       bg="brand.flatBlack" 
       color="brand.white"
       height="calc(100vh - 50px)"
-      // overflow="auto"
       overflow='hidden'
       >
       <Router>
--- src/pages/Help/Help.tsx
+++ src/pages/Help/Help.tsx
@@ -127,12 +127,3 @@ export default function Help() {
 
     )
 }
-
-/*
-       <Container maxW="37.5rem">
-            <Box bg="brand.darkGrey" borderRadius="16px" pb={4} my={10} h="calc(100vh - 160px)" w="100%">
-                <Heading as="h2" m={4} py={4}> Help Page coming soon</Heading>
-                <Text as="p" mx={4}> This page will contain detailed intructions on how to use the tuning guide.</Text>
-            </Box>
-        </Container>
-        */
