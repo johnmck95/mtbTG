e09d150ee62a87be5bf2f00fdc09701735cec5c9
John <john.mic.mckinnon@gmail.com>
2022-06-12 18:44:33 -0700

    Setup tags for the help page to link directly the the required section. There is one bug where the auto-scroll can cut off part of the search bar

--- src/pages/Help/Help.tsx
+++ src/pages/Help/Help.tsx
@@ -65,32 +65,60 @@ export default function Help() {
                             />
                         </HStack>
 
-                        <Heading as='h3' fontSize={"md"}>
-                            <Icon as={FaHandsHelping} w={4} h={4} mr="0.75rem"/>
-                            Using The Tuning Guide
-                        </Heading>
-
-                        <Heading as='h3' fontSize={"md"}>
-                            <Icon as={FaTools} w={4} h={4} mr="0.75rem"/>
-                            Understanding Inputs
-                        </Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Rider Size</Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Handling</Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Skill Level</Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Reach</Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Stack</Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Bike Type</Heading>
-
-                        <Heading as='h3' fontSize={"md"}>
-                            <Icon as={FaBicycle} w={4} h={4} mr="0.75rem"/>
-                            Your Setting
-                        </Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Handlebar Width</Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Handlebar Rise</Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Stem Length</Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Stem Spacers</Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Tire Pressures</Heading>
-                        <Heading as='h4' fontSize={"sm"} pl='2rem'>Tire Inserts</Heading>
+                        <a href="#usingTheTuningGuide">
+                            <Heading as='h3' fontSize={"md"}>
+                                <Icon as={FaHandsHelping} w={4} h={4} mr="0.75rem"/>
+                                Using The Tuning Guide
+                            </Heading>
+                        </a>
+                        <a href='#understandingInputs'>
+                            <Heading as='h3' fontSize={"md"}>
+                                <Icon as={FaTools} w={4} h={4} mr="0.75rem"/>
+                                Understanding Inputs
+                            </Heading>
+                        </a>
+                        <a href="#riderSize">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Rider Size</Heading>
+                        </a>
+                        <a href="#handling">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Handling</Heading>
+                        </a>
+                        <a href="#skillLevel">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Skill Level</Heading>
+                        </a>
+                        <a href="#reach">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Reach</Heading>
+                        </a>
+                        <a href="#stack">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Stack</Heading>
+                        </a>
+                        <a href="#bikeType">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Bike Type</Heading>
+                        </a>
+                        <a href="#yourSettings">
+                            <Heading as='h3' fontSize={"md"}>
+                                <Icon as={FaBicycle} w={4} h={4} mr="0.75rem"/>
+                                Your Setting
+                            </Heading>
+                        </a>
+                        <a href="#handlebarWidth">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Handlebar Width</Heading>
+                        </a>
+                        <a href="#handlebarRise">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Handlebar Rise</Heading>
+                        </a>
+                        <a href="#stemLength">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Stem Length</Heading>
+                        </a>
+                        <a href="#stemSpacers">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Stem Spacers</Heading>
+                        </a>
+                        <a href="#tirePressures">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Tire Pressures</Heading>
+                        </a>
+                        <a href="#tireInserts">
+                            <Heading as='h4' fontSize={"sm"} pl='2rem'>Tire Inserts</Heading>
+                        </a>
                     </VStack>
                 }
                 {!showPanel && 
@@ -116,38 +144,66 @@ export default function Help() {
                        'scrollbarWidth': 'none',
                    }}
                     >
-                        <Heading as='h1' fontSize={"4xl"} >Using The Tuning Guide</Heading>
+                        <a id="usingTheTuningGuide">
+                            <Heading as='h1' fontSize={"4xl"} >Using The Tuning Guide</Heading>
+                        </a>
                         <Divider />
                         <Text>{hipsterIpsum}</Text>
-
-                        <Heading as='h1' fontSize={"4xl"} >Understanding Inputs</Heading>
+                        <a id="understandingInputs">
+                            <Heading as='h1' fontSize={"4xl"} >Understanding Inputs</Heading>
+                        </a>
                         <Divider />
-                        <Heading as='h2' fontSize={"2xl"} >Rider Size</Heading>
+                        <a id="riderSize">
+                            <Heading as='h2' fontSize={"2xl"} >Rider Size</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
-                        <Heading as='h2' fontSize={"2xl"} >Handling</Heading>
+                        <a id="handling">
+                            <Heading as='h2' fontSize={"2xl"} >Handling</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
-                        <Heading as='h2' fontSize={"2xl"} >Skill Level</Heading>
+                        <a id="skillLevel">
+                            <Heading as='h2' fontSize={"2xl"} >Skill Level</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
-                        <Heading as='h2' fontSize={"2xl"} >Reach</Heading>
+                        <a id="reach">
+                            <Heading as='h2' fontSize={"2xl"} >Reach</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
-                        <Heading as='h2' fontSize={"2xl"} >Stack</Heading>
+                        <a id="stack">
+                            <Heading as='h2' fontSize={"2xl"} >Stack</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
-                        <Heading as='h2' fontSize={"2xl"} >Bike Type</Heading>
+                        <a id="bikeType">
+                            <Heading as='h2' fontSize={"2xl"} >Bike Type</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
-
-                        <Heading as='h1' fontSize={"4xl"} >Your Settings</Heading>
+                        <a id="yourSettings">
+                            <Heading as='h1' fontSize={"4xl"} >Your Settings</Heading>
+                        </a>
                         <Divider />
-                        <Heading as='h2' fontSize={"2xl"} >HandleBar Width</Heading>
+                        <a id="handlebarWidth">
+                            <Heading as='h2' fontSize={"2xl"} >HandleBar Width</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
-                        <Heading as='h2' fontSize={"2xl"} >Handlebar Rise</Heading>
+                        <a id="handlebarRise">
+                            <Heading as='h2' fontSize={"2xl"} >Handlebar Rise</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
-                        <Heading as='h2' fontSize={"2xl"} >Stem Length</Heading>
+                        <a id="stemLength">
+                            <Heading as='h2' fontSize={"2xl"} >Stem Length</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
-                        <Heading as='h2' fontSize={"2xl"} >Stem Spacers</Heading>
+                        <a id="stemSpacers">
+                            <Heading as='h2' fontSize={"2xl"} >Stem Spacers</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
-                        <Heading as='h2' fontSize={"2xl"} >Tire Pressures</Heading>
+                        <a id="tirePressures">
+                            <Heading as='h2' fontSize={"2xl"} >Tire Pressures</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
-                        <Heading as='h2' fontSize={"2xl"} >Tire Inserts</Heading>
+                        <a id="tireInserts">
+                            <Heading as='h2' fontSize={"2xl"} >Tire Inserts</Heading>
+                        </a>
                         <Text>{hipsterIpsum}</Text>
                 </VStack>
             </HStack>
