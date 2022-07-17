4c7cfc2bab3b7942e3b3b04ca7e40f21e2eae120
John <john.mic.mckinnon@gmail.com>
2022-07-16 19:47:48 -0700

    Update manifest.json to have a colour match with theme.ts

--- src/styling/theme.ts
+++ src/styling/theme.ts
@@ -10,6 +10,7 @@ export const theme = extendTheme({
             black: "black",
             lightBlack: "#222222",
             // flatBlack: "#262626", // Original choice
+            /* Must manually update manifest.json if background colour changes */
             flatBlack: "rgba(0, 0, 0, 0.80)", // Testing this colour
             headerGradient: "linear-gradient(to top, #222222, black 15%)",
             error: "#d5798a",
